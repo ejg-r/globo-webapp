08f3141d2a68f97147927e30c290acc3259c3d58
Eduardo Rodrigues <ejg.rodrigues@hotmail.com>
2024-08-09 15:42:01 +0100

    update SG

--- security_groups.tf
+++ security_groups.tf
@@ -2,42 +2,53 @@
 # RESOURCES
 ##################################################################################
 
-resource "aws_security_group" "webapp_http_inbound_sg" {
-  name        = "${local.name_prefix}-http-inbound"
-  description = "Allow HTTP from Anywhere"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+resource "aws_security_group" "webapp_sg" {
+  name        = "Webapp Security Group"
+  description = "Ingress and Egress are outside"
   vpc_id = data.tfe_outputs.networking.nonsensitive_values.vpc_id
+  tags = local.common_tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "webapp_http_inbound" {
+  security_group_id = aws_security_group.webapp_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
 
   tags = local.common_tags
 }
 
+resource "aws_vpc_security_group_ingress_rule" "webapp_https_inbound" {
+  security_group_id = aws_security_group.webapp_sg.id
 
-resource "aws_security_group" "webapp_outbound_sg" {
-  name        = "${local.name_prefix}-webapp-outbound"
-  description = "Allow outbound connections"
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  vpc_id = data.tfe_outputs.networking.nonsensitive_values.vpc_id
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
 
   tags = local.common_tags
+}
+
+resource "aws_vpc_security_group_egress_rule" "webapp_outbound" {
+  security_group_id = aws_security_group.webapp_sg.id
+
+  from_port = 0
+  to_port = 0
+  ip_protocol = "-1"
+  cidr_ipv4 = "0.0.0.0/0"
+
+  tags = local.common_tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh_rule" {
+  security_group_id = aws_security_group.example.id
+
+  from_port = 22
+  to_port = 22
+  ip_protocol = "tcp"
+  cidr_ipv4 = "94.60.110.242/24"
+
+  description       = "Allow SSH from specific IP"
 }
