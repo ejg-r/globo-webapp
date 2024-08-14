7c3f98236b38430dd8f4c58bf462d32d7173eb1e
Eduardo Rodrigues <ejg.rodrigues@hotmail.com>
2024-08-14 17:59:06 +0100

    revert userdata.sh

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -3,12 +3,6 @@ sudo yum update
 sudo yum install git -y
 sudo amazon-linux-extras install ansible2 -y
 sudo mkdir /var/ansible_playbooks
-#sudo git clone ${playbook_repository} /var/ansible_playbooks
-
-sudo chown -R ec2-user /var
-sudo chown -R ec2-user /home
-
-eval "$(ssh-agent -s)"
 
 # Setup SSH key for GitHub access
 echo "${private_key}" > /home/ec2-user/.ssh/id_rsa
@@ -17,9 +11,6 @@ chmod 600 /home/ec2-user/.ssh/id_rsa
 ssh-keyscan github.com >> /home/ec2-user/.ssh/known_hosts
 # Clone the private repository
 sudo git clone ${playbook_repository} /var/ansible_playbooks
-# Delete the SSH key after cloning
-shred -u /home/ec2-user/.ssh/id_rsa
-
 
 aws secretsmanager get-secret-value --secret-id "${secret_id}" --region us-east-1 --query SecretString --output text > /var/ansible_playbooks/api_key.txt
 aws ssm get-parameter --name "${host_list_ssm_name}" --region us-east-1 --query Parameter.Value --output text > /var/ansible_playbooks/host_list.txt
