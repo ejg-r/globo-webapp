e10d040996146bd6911167cf4d7126eb27ab90b0
Eduardo Rodrigues <ejg.rodrigues@hotmail.com>
2024-08-16 17:08:37 +0100

    removed debud echo

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -6,7 +6,6 @@ sudo mkdir /var/ansible_playbooks
 
 
 git_secret_id_temp= aws secretsmanager get-secret-value --secret-id "${git_secret_id}" --region us-east-1 --query SecretString --output text
-echo $git_secret_id_temp > /var/ansible_playbooks/git_key.txt
 repo_url="https://$git_secret_id_temp/${playbook_repository}"
 
 
